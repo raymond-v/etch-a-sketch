17028f2d0c4c7f6880ce2eba1e8b44aef55fe887
Raymond <raymond.vuong@torontomu.ca>
2026-09-12 18:10:39 -0400

    Set grid display limit

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ const button = document.querySelector('button')
 
 button.addEventListener('click', () => {
     let answer = prompt('Number of squares')
-    if (answer) {
+    if (answer > 0 && answer <= 100) {
         div.innerHTML = ''
         div.style.width = `${50 * answer}px`
         for (let i = 0; i < answer * answer; i++) {
