378f19fc7959cefe4b31330f7d44f9110993b465
Raymond <raymond.vuong@torontomu.ca>
2026-09-12 18:01:44 -0400

    Add prompt and working hover

--- script.js
+++ script.js
@@ -12,3 +12,22 @@ allDiv.forEach(element => {
         element.style.backgroundColor = 'black'
     })
 })
+
+const button = document.querySelector('button')
+
+button.addEventListener('click', () => {
+    let answer = prompt('Number of squares')
+    if (answer) {
+        div.innerHTML = ''
+        for (let i = 0; i < answer * answer; i++) {
+            let newDiv = document.createElement('div')
+            div.appendChild(newDiv)
+        }
+        const allDiv = document.querySelectorAll('.container div')
+        allDiv.forEach(element => {
+            element.addEventListener('mouseenter', () => {
+                element.style.backgroundColor = 'black'
+            })
+        })
+    }
+})
